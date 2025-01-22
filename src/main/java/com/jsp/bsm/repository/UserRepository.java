0b0c8c2ab59b6914e53d443f970f96920d950538
MD JUNED ALAM <mdjunedalam220@gmail.com>
2025-01-22 15:25:50 +0530

    creating the userRepository interface for the database logic and performing the CRUD operation in the project

--- src/main/java/com/jsp/bsm/repository/UserRepository.java
+++ src/main/java/com/jsp/bsm/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.jsp.bsm.repository;
 
-public interface UserRepository {
+import com.jsp.bsm.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
