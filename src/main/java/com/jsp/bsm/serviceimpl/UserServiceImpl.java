bcef366fddd4336a00ce2f5adc3f380e8323bad2
MD JUNED ALAM <mdjunedalam220@gmail.com>
2025-01-22 15:22:29 +0530

    creating the serviceimplemention class  and adding the methodsimplementation  for adding the users and finding the userd by id and updating the userd

--- src/main/java/com/jsp/bsm/serviceimpl/UserServiceImpl.java
+++ src/main/java/com/jsp/bsm/serviceimpl/UserServiceImpl.java
@@ -1,4 +1,31 @@
 package com.jsp.bsm.serviceimpl;
 
-public class UserServiceImpl {
+import com.jsp.bsm.entity.User;
+import com.jsp.bsm.repository.UserRepository;
+import com.jsp.bsm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+//    @Override
+//    public Optional<User> findById(int userId) {
+//        return  userRepository.findById(userId);
+//    }
+//
+//    @Override
+//    public User updateUserById(User user) {
+//        return userRepository.save(user);
+ //   }
 }
