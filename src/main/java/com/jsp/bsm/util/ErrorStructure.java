3dbeb4d635532137b87aec9ed5d8f4c45d21a823
MD JUNED ALAM <mdjunedalam220@gmail.com>
2025-01-22 16:15:23 +0530

    creating the error struction class and defines the methods which accept parameters

--- src/main/java/com/jsp/bsm/util/ErrorStructure.java
+++ src/main/java/com/jsp/bsm/util/ErrorStructure.java
@@ -1,4 +1,19 @@
 package com.jsp.bsm.util;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ErrorStructure {
+//    public static <T> ResponseEntity<T> builder() {
+//
+//    }
+    private int status;
+    private String message;
+    private String rootCause;
 }
