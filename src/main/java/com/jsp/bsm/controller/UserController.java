d75784b04af287b55cecb6bb109b1ac7459341d3
MD JUNED ALAM <mdjunedalam220@gmail.com>
2025-01-22 16:22:20 +0530

    creating the register ending point

--- src/main/java/com/jsp/bsm/controller/UserController.java
+++ src/main/java/com/jsp/bsm/controller/UserController.java
@@ -2,6 +2,8 @@ package com.jsp.bsm.controller;
 
 import com.jsp.bsm.entity.User;
 import com.jsp.bsm.service.UserService;
+import com.jsp.bsm.util.ResponseStructure;
+import com.jsp.bsm.util.RestResponseBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,11 +16,18 @@ public class UserController {
 
 @Autowired
     private UserService userService;
+    private RestResponseBuilder restResponseBuildere;
+
     @PostMapping("/register")
-        public ResponseEntity<User> addUser(@RequestBody User user) {
-            User createdUser = userService.addUser(user);
-            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+//        public ResponseEntity<User> addUser(@RequestBody User user) {
+//            User createdUser = userService.addUser(user);
+//            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user) {
+        user = userService.addUser(user);
+        return restResponseBuildere.success(HttpStatus.CREATED, "User Created", user);
+    }
         }
 
-    }
+
 
