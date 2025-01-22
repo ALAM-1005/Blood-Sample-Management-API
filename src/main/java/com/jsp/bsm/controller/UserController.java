fef16419945a5f9b30e2150a10178a3a4c6c9613
MD JUNED ALAM <mdjunedalam220@gmail.com>
2025-01-22 15:17:44 +0530

    creating the user control class for pass the ending  point of api

--- src/main/java/com/jsp/bsm/controller/UserController.java
+++ src/main/java/com/jsp/bsm/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.jsp.bsm.controller;
 
+import com.jsp.bsm.entity.User;
+import com.jsp.bsm.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
-}
+
+@Autowired
+    private UserService userService;
+    @PostMapping("/register")
+        public ResponseEntity<User> addUser(@RequestBody User user) {
+            User createdUser = userService.addUser(user);
+            return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+        }
+
+    }
+
